50d15614abca3430f06b88ef3c30556c4999ed79
Jonathan Beliën <jbe@geo6.be>
2018-11-23 11:09:15 +0100

    Update draw.js

--- resources/javascript/draw.js
+++ resources/javascript/draw.js
@@ -3,8 +3,8 @@ import DrawControl from './map/draw';
 export default function () {
     window.app.draw = new DrawControl();
 
-    $('#sidebar a[href="#draw"]').on('click', () => {
-        window.app.draw.active = !$('#sidebar').hasClass('collapsed');
+    $('#sidebar a').on('click', () => {
+        window.app.draw.active = $('#sidebar > .sidebar-tabs > ul > li:has(a[href="#draw"])').hasClass('active');
 
         if (window.app.draw.active === false) {
             window.app.draw.disable();
