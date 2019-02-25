9f3b24ee68ef3629a5d6d1a698eebbf36907a439
Jonathan Beliën <dev@jbelien.be>
2019-02-25 16:31:24 +0100

    Update AddressHandler.php

--- src/App/Handler/Geocoder/AddressHandler.php
+++ src/App/Handler/Geocoder/AddressHandler.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace App\Handler\Geocoder;
 
 use App\Middleware\ConfigMiddleware;
+use ErrorException;
 use Geocoder\Dumper\GeoJson;
 use Geocoder\Formatter\StringFormatter;
 use Geocoder\Query\GeocodeQuery;
+use Geocoder\Provider\Provider;
 use Geocoder\StatefulGeocoder;
 use GuzzleHttp\Client as GuzzleClient;
 use Http\Adapter\Guzzle6\Client as GuzzleAdapter;
@@ -18,6 +20,14 @@ use Zend\Diactoros\Response\JsonResponse;
 
 class AddressHandler implements RequestHandlerInterface
 {
+    /**
+     * Given a Geocoder provider name and an address as parameters,
+     * geocode the address and return the matched locations.
+     *
+     * @param ServerRequestInterface $request
+     *
+     * @return ResponseInterface
+     */
     public function handle(ServerRequestInterface $request) : ResponseInterface
     {
         $config = $request->getAttribute(ConfigMiddleware::CONFIG_ATTRIBUTE);
@@ -30,88 +40,117 @@ class AddressHandler implements RequestHandlerInterface
         ]);
         $adapter = new GuzzleAdapter($guzzle);
 
-        switch ($provider) {
+        $geocoder = self::getGeocoder(
+            $provider,
+            $adapter,
+            $config['geocoder']['providers'][$provider]['customerId'] ?? null,
+            $config['geocoder']['providers'][$provider]['customerId'] ?? null
+        );
+
+        $locations = self::geocode($geocoder, $address);
+
+        return new JsonResponse([
+            'type'     => 'FeatureCollection',
+            'features' => $locations,
+        ]);
+    }
+
+    /**
+     * Instantiate the Geocoder Provider.
+     *
+     * @param string $name Provider name.
+     * @param GuzzleAdapter $adapter
+     * @param string|null $customerId
+     * @param string|null $privateKey
+     *
+     * @return Provider
+     */
+    private static function getGeocoder(
+        string $name,
+        GuzzleAdapter $adapter,
+        ? string $customerId = null,
+        ? string $privateKey = null
+    ) : Provider {
+        switch ($name) {
             case 'bpost':
-                $geocoder = new \Geocoder\Provider\bpost\bpost($adapter);
-                break;
+                return new \Geocoder\Provider\bpost\bpost($adapter);
 
             case 'geo6':
-                $geocoder = new \Geocoder\Provider\Geo6\Geo6(
+                return new \Geocoder\Provider\Geo6\Geo6(
                     $adapter,
-                    $config['geocoder']['providers']['geo6']['customerId'] ?? '',
-                    $config['geocoder']['providers']['geo6']['privateKey'] ?? ''
+                    $customerId ?? '',
+                    $privateKey ?? ''
                 );
-                break;
 
             case 'geo6-poi':
-                $geocoder = new \Geocoder\Provider\Geo6\POI\Geo6POI(
+                return new \Geocoder\Provider\Geo6\POI\Geo6POI(
                     $adapter,
-                    $config['geocoder']['providers']['geo6-poi']['customerId'] ?? '',
-                    $config['geocoder']['providers']['geo6-poi']['privateKey'] ?? ''
+                    $customerId ?? '',
+                    $privateKey ?? ''
                 );
-                break;
 
             case 'geopunt':
-                $geocoder = new \Geocoder\Provider\Geopunt\Geopunt($adapter);
-                break;
+                return new \Geocoder\Provider\Geopunt\Geopunt($adapter);
 
             case 'nominatim':
-                $geocoder = \Geocoder\Provider\Nominatim\Nominatim::withOpenStreetMapServer(
+                return \Geocoder\Provider\Nominatim\Nominatim::withOpenStreetMapServer(
                     $adapter,
                     $_SERVER['HTTP_USER_AGENT']
                 );
-                break;
 
             case 'spw':
-                $geocoder = new \Geocoder\Provider\SPW\SPW($adapter);
-                break;
+                return new \Geocoder\Provider\SPW\SPW($adapter);
 
             case 'urbis':
-                $geocoder = new \Geocoder\Provider\UrbIS\UrbIS($adapter);
-                break;
+                return new \Geocoder\Provider\UrbIS\UrbIS($adapter);
+
+            default:
+                throw new ErrorException(sprintf('The "%s" provider is not installed or configured.', $name));
         }
+    }
 
-        $locations = [
-            'type'     => 'FeatureCollection',
-            'features' => [],
-        ];
+    /**
+     * Geocode an address with a given Geocoder provider.
+     *
+     * @param Provider $geocoder
+     * @param string $address
+     *
+     * @return array
+     */
+    private static function geocode(Provider $geocoder, string $address) : array
+    {
+        $query = GeocodeQuery::create($address);
 
-        if (isset($geocoder)) {
-            $query = GeocodeQuery::create($address);
+        $result = (new StatefulGeocoder($geocoder))->geocodeQuery($query);
 
-            $result = (new StatefulGeocoder($geocoder))
-                ->geocodeQuery($query);
+        $locations = [];
 
-            $dumper = new GeoJson();
-            $formatter = new StringFormatter();
+        foreach ($result->all() as $location) {
+            $json = json_decode((new GeoJson())->dump($location));
 
-            foreach ($result->all() as $location) {
-                $json = json_decode($dumper->dump($location));
+            switch ($geocoder->getName()) {
+                case 'geo6-poi':
+                    $json->properties->type = $location->getType();
+                    $json->properties->formattedAddress = sprintf(
+                        '%s: %s',
+                        $location->getType(),
+                        $location->getName()
+                    );
+                    break;
 
-                switch ($provider) {
-                    case 'geo6-poi':
-                        $json->properties->type = $location->getType();
-                        $json->properties->formattedAddress = sprintf(
-                            '%s: %s',
-                            $location->getType(),
-                            $location->getName()
-                        );
-                        break;
+                case 'nominatim':
+                    $json->properties->type = $location->getType();
+                    $json->properties->formattedAddress = $location->getDisplayName();
+                    break;
 
-                    case 'nominatim':
-                        $json->properties->type = $location->getType();
-                        $json->properties->formattedAddress = $location->getDisplayName();
-                        break;
-
-                    default:
-                        $json->properties->formattedAddress = $formatter->format($location, '%S %n, %z %L');
-                        break;
-                }
-
-                $locations['features'][] = $json;
+                default:
+                    $json->properties->formattedAddress = (new StringFormatter())->format($location, '%S %n, %z %L');
+                    break;
             }
+
+            $locations[] = $json;
         }
 
-        return new JsonResponse($locations);
+        return $locations;
     }
 }
