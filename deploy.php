2adb00750d3cf11dd0aba8b3535e5e160000d3a6
Jonathan Beliën <jbe@geo6.be>
2019-08-08 16:01:15 +0200

    🚀 Create deploy.php (Deployer)

---
+++ deploy.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/zend_framework.php';
+
+// Project name
+set('application', 'mapper');
+
+// Project repository
+set('repository', 'git@github.com:geo6/mapper.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+add('shared_files', [
+    'config/autoload/local.php',
+]);
+add('shared_dirs', [
+    'config/application',
+]);
+
+// Writable dirs by web server
+add('writable_dirs', [
+    'config',
+    'data/cache',
+]);
+set('allow_anonymous_stats', false);
+
+// Hosts
+
+inventory('hosts.yml');
+
+// Tasks
+
+task('debug:enable', 'composer run development-enable');
+task('debug:disable', 'composer run development-disable');
+
+task('php:version', function () {
+    $test = run('php -r "echo version_compare(PHP_VERSION, \'7.3\', \'<\');"');
+
+    if ($test == 1) {
+        run('cd {{ release_path }} && composer update');
+    }
+});
+after('deploy:vendors', 'php:version');
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
