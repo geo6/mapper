70049c040f3b2af6e0671f3ad73e6481333d47ee
Jonathan Beliën <jbe@geo6.be>
2020-04-17 14:43:57 +0200

    Update marker.js
    Improve marker (highlight) style.

--- resources/javascript/map/marker.js
+++ resources/javascript/map/marker.js
@@ -4,8 +4,17 @@ import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
+import { Style, Circle, Fill, Stroke } from 'ol/style';
 
 export default function () {
+    var fill = new Fill({
+        color: 'rgba(255,0,0,0.1)'
+    });
+    var stroke = new Stroke({
+        color: 'red',
+        width: 5
+    });
+
     window.app.marker = new Feature({
         geometry: new Point([0, 0])
     });
@@ -14,6 +23,17 @@ export default function () {
         source: new VectorSource({
             features: [window.app.marker]
         }),
+        style: [
+            new Style({
+                image: new Circle({
+                    fill: fill,
+                    stroke: stroke,
+                    radius: 5
+                }),
+                fill: fill,
+                stroke: stroke
+            })
+        ],
         visible: false,
         zIndex: Infinity
     });
