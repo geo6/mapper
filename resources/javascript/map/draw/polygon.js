d6d97fd51830ae9910985ae8eed92f597adcbdfb
Jonathan Beliën <jbe@geo6.be>
2020-04-20 12:05:21 +0200

    Update polygon.js
    Remove self-intersection in drawn polygons.
    Close #147

--- resources/javascript/map/draw/polygon.js
+++ resources/javascript/map/draw/polygon.js
@@ -1,6 +1,9 @@
 'use strict';
 
+import GeoJSON from 'ol/format/GeoJSON';
+import MultiPolygon from 'ol/geom/MultiPolygon';
 import Draw from 'ol/interaction/Draw';
+import unkinkPolygon from '@turf/unkink-polygon';
 
 class DrawPolygon extends Draw {
     constructor () {
@@ -10,9 +13,34 @@ class DrawPolygon extends Draw {
             type: 'Polygon'
         });
 
-        this.on('drawend', () => {
+        this.on('drawend', (event) => {
+            this.validate(event.feature);
+
             window.app.draw.showForm();
         });
+    }
+
+    validate (feature) {
+        const geojson = (new GeoJSON()).writeFeature(feature, {
+            dataProjection: 'EPSG:4326',
+            decimals: 6,
+            featureProjection: window.app.map.getView().getProjection()
+        });
+
+        const valid = unkinkPolygon(JSON.parse(geojson));
+
+        const features = (new GeoJSON()).readFeatures(valid, {
+            dataProjection: 'EPSG:4326',
+            featureProjection: window.app.map.getView().getProjection()
+        });
+
+        if (features.length > 1) {
+            const coordinates = features.map(feature => feature.getGeometry().getCoordinates());
+
+            feature.setGeometry(new MultiPolygon(coordinates));
+        }
+
+        return feature;
     }
 }
 
