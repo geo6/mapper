8092022585031e39cf39ff86d205d282d34ff4d8
Jonathan Beliën <jbe@geo6.be>
2020-04-27 12:15:08 +0200

    Refactor layer settings modal

--- resources/javascript/modal/SettingsModal.ts
+++ resources/javascript/modal/SettingsModal.ts
@@ -1,17 +1,41 @@
 "use strict";
 
 export class SettingsModal {
-  layer = null;
+  private element: HTMLElement;
+  private form: HTMLFormElement;
+
+  private labelSelect: HTMLSelectElement;
+  private colorInput: HTMLInputElement;
+  private colorInputText: HTMLElement;
+
+  private layer = null;
 
   constructor(element: HTMLElement) {
-    element.querySelector("form").addEventListener("submit", (event: Event) => {
+    this.element = element;
+    this.form = this.element.querySelector("form");
+
+    this.labelSelect = document.getElementById(
+      "layer-label"
+    ) as HTMLSelectElement;
+    this.colorInput = document.getElementById(
+      "layer-color"
+    ) as HTMLInputElement;
+    this.colorInputText = document.getElementById(
+      "layer-color-text"
+    ) as HTMLElement;
+
+    this.form.addEventListener("reset", () => {
+      this.labelSelect.disabled = false;
+      this.colorInput.disabled = false;
+
+      this.colorInputText.hidden = true;
+    });
+
+    this.form.addEventListener("submit", (event: Event) => {
       event.preventDefault();
 
-      const labelInput = document.getElementById(
-        "layer-label"
-      ) as HTMLInputElement;
-
-      this.layer.label = labelInput.value.length > 0 ? labelInput.value : null;
+      this.layer.label = this.getLabel();
+      this.layer.color = this.getColor();
 
       const colorInput = document.getElementById(
         "layer-color"
@@ -26,8 +50,82 @@ export class SettingsModal {
 
       this.layer = null;
 
-      $(element).modal("hide");
+      this.hide();
     });
+  }
+
+  show(): void {
+    $(this.element).modal("show");
+  }
+  hide(): void {
+    $(this.element).modal("hide");
+  }
+
+  reset(): void {
+    this.form.reset();
+  }
+
+  setLayer(layer, name: string): void {
+    this.layer = layer;
+
+    (this.element.querySelector(
+      ".modal-body > span.font-weight-bold"
+    ) as HTMLSpanElement).innerText = name;
+
+    this.setLabelList(
+      this.layer.getColumns().filter((column: string) => column !== "geometry")
+    );
+    this.setLabel(layer.label);
+
+    if (
+      layer.type === "geojson" &&
+      typeof layer.content.legend === "object" &&
+      Array.isArray(layer.content.legend)
+    ) {
+      this.disableColor("Function disabled because this layer has a legend.");
+    } else if (layer.type === "kml") {
+      this.disableColor("Function disabled for KML files.");
+    } else {
+      this.setColor(layer.color);
+    }
+  }
+
+  setLabelList(labels: string[]): void {
+    this.labelSelect.innerHTML = '<option value=""></option>';
+    labels.forEach((column: string) => {
+      const option = document.createElement("option");
+
+      option.value = column;
+      option.innerText = column;
+
+      this.labelSelect.append(option);
+    });
+  }
+
+  setLabel(label: string): void {
+    this.labelSelect.value = label;
+  }
+  getLabel(): string | null {
+    return this.labelSelect.value.length > 0 ? this.labelSelect.value : null;
+  }
+
+  disableColor(message?: string): void {
+    this.colorInput.disabled = true;
+
+    if (typeof message !== "undefined") {
+      this.colorInputText.innerText = message;
+      this.colorInputText.hidden = false;
+    }
+  }
+  setColor(color: string): void {
+    this.colorInput.value = color;
+  }
+  getColor(): string | null {
+    if (this.colorInput.disabled === false) {
+      return this.colorInput.value.length > 0 ? this.colorInput.value : null;
+    }
+
+    return null;
   }
 }
 
--- resources/javascript/sidebar/layers/components/btn-settings.ts
+++ resources/javascript/sidebar/layers/components/btn-settings.ts
@@ -18,61 +18,9 @@ export default function (
     button.addEventListener("click", (event: Event) => {
       event.preventDefault();
 
-      const layer = window.app[type][index];
-      const columns = layer.getColumns();
-
-      modalSettings.layer = window.app[type][index];
-
-      (document.querySelector(
-        "#modal-settings .modal-body > span.font-weight-bold"
-      ) as HTMLSpanElement).innerText = name;
-
-      document.getElementById("layer-label").innerHTML =
-        '<option value=""></option>';
-      columns
-        .filter((column: string) => column !== "geometry")
-        .forEach((column: string) => {
-          const option = document.createElement("option");
-          option.value = column;
-          option.innerText = column;
-          option.selected = column === layer.label;
-
-          document.getElementById("layer-label").append(option);
-        });
-
-      const colorInput = document.getElementById(
-        "layer-color"
-      ) as HTMLInputElement;
-      const colorInputText = document.getElementById(
-        "layer-color-text"
-      ) as HTMLElement;
-
-      if (
-        layer.type === "geojson" &&
-        typeof layer.content.legend === "object" &&
-        Array.isArray(layer.content.legend)
-      ) {
-        colorInput.disabled = true;
-        colorInput.value = "";
-
-        colorInputText.innerText =
-          "Function disabled because this layer has a legend.";
-        colorInputText.hidden = false;
-      } else if (layer.type === "kml") {
-        colorInput.disabled = true;
-        colorInput.value = "";
-
-        colorInputText.innerText = "Function disabled for KML files.";
-        colorInputText.hidden = false;
-      } else {
-        colorInput.disabled = false;
-        colorInput.value = layer.color;
-
-        colorInputText.innerText = "";
-        colorInputText.hidden = true;
-      }
-
-      $("#modal-settings").modal("show");
+      modalSettings.reset();
+      modalSettings.setLayer(window.app[type][index], name);
+      modalSettings.show();
     });
   }
 
