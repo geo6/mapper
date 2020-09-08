ad91909dae68f923e752817ff3ccc123b8e52b48
Jonathan Beliën <jbe@geo6.be>
2020-09-08 11:22:48 +0200

    Update DrawPolygon.ts
    Display area while drawing (#97).

--- resources/javascript/map/draw/DrawPolygon.ts
+++ resources/javascript/map/draw/DrawPolygon.ts
@@ -1,16 +1,22 @@
 "use strict";
 
+import { EventsKey } from "ol/events";
 import Feature from "ol/Feature";
 import GeoJSON from "ol/format/GeoJSON";
 import GeometryType from "ol/geom/GeometryType";
 import MultiPolygon from "ol/geom/MultiPolygon";
-import Draw from "ol/interaction/Draw";
+import Draw, { DrawEvent } from "ol/interaction/Draw";
+import { unByKey } from "ol/Observable";
+import { getArea } from "ol/sphere";
 import unkinkPolygon from "@turf/unkink-polygon";
 
 import { map } from "../../main";
 import { drawControl } from "../../sidebar/draw";
 
 class DrawPolygon extends Draw {
+  private listener: EventsKey;
+  private element: HTMLElement;
+
   constructor() {
     super({
       source: drawControl.layerCurrent.getSource(),
@@ -18,7 +24,19 @@ class DrawPolygon extends Draw {
       type: "Polygon" as GeometryType,
     });
 
+    this.element = document.querySelector(".ol-measure-result");
+
+    this.on("drawstart", (event: DrawEvent) => {
+      this.listener = event.feature.on("change", () => {
+        this.showArea(event.feature);
+      });
+    });
+
     this.on("drawend", (event) => {
+      unByKey(this.listener);
+
+      this.element.innerHTML = "";
+
       this.validate(event.feature);
 
       drawControl.showForm();
@@ -48,6 +66,22 @@ class DrawPolygon extends Draw {
     }
 
     return feature;
+  }
+
+  showArea(feature: Feature): number {
+    const area = getArea(feature.getGeometry());
+
+    this.element.innerHTML = DrawPolygon.formatArea(area);
+
+    return area;
+  }
+
+  static formatArea(area: number): string {
+    if (area > 10000) {
+      return Math.round((area / 1000000) * 100) / 100 + " " + "km<sup>2</sup>";
+    } else {
+      return Math.round(area * 100) / 100 + " " + "m<sup>2</sup>";
+    }
   }
 }
 
