67ca339d5a028b8b7ee930a6aaad73911ce5f514
Jonathan Beliën <jbelien@users.noreply.github.com>
2020-09-08 11:51:05 +0200

    Display length/area while drawing (#308)
    * Update DrawLineString.ts
    
    Display length while drawing (#97).
    
    * Update DrawPolygon.ts
    
    Display area while drawing (#97).
    
    * Rename this.element to this.target
    
    * 🚨 Update DrawLineString.ts

--- resources/javascript/map/draw/DrawLineString.ts
+++ resources/javascript/map/draw/DrawLineString.ts
@@ -1,11 +1,18 @@
 "use strict";
 
-import Draw from "ol/interaction/Draw";
+import { EventsKey } from "ol/events";
+import Feature from "ol/Feature";
 import GeometryType from "ol/geom/GeometryType";
+import Draw, { DrawEvent } from "ol/interaction/Draw";
+import { unByKey } from "ol/Observable";
+import { getLength } from "ol/sphere";
 
 import { drawControl } from "../../sidebar/draw";
 
 class DrawLineString extends Draw {
+  private listener: EventsKey;
+  private target: HTMLElement;
+
   constructor() {
     super({
       source: drawControl.layerCurrent.getSource(),
@@ -13,9 +20,37 @@ class DrawLineString extends Draw {
       type: "LineString" as GeometryType,
     });
 
+    this.target = document.querySelector(".ol-measure-result");
+
+    this.on("drawstart", (event: DrawEvent) => {
+      this.listener = event.feature.on("change", () => {
+        this.showLength(event.feature);
+      });
+    });
+
     this.on("drawend", () => {
+      unByKey(this.listener);
+
+      this.target.innerHTML = "";
+
       drawControl.showForm();
     });
+  }
+
+  showLength(feature: Feature): number {
+    const length = getLength(feature.getGeometry());
+
+    this.target.innerHTML = DrawLineString.formatLength(length);
+
+    return length;
+  }
+
+  static formatLength(length: number): string {
+    if (length >= 1000) {
+      return Math.round((length / 1000) * 1000) / 1000 + " " + "km";
+    } else {
+      return Math.round(length * 100) / 100 + " " + "m";
+    }
   }
 }
 
